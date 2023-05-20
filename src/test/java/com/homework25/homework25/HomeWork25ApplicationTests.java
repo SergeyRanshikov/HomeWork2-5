1d789f80dbd87181ac69a04e7be1f70cb7548506
ranss@rambler.ru <ranss@rambler.ru>
2023-05-20 15:39:02 +0300

    Начальный коммит

---
+++ src/test/java/com/homework25/homework25/HomeWork25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework25.homework25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
